25916a3f351e3658315e55d0cb7e4618fec5a926
Huy Pham <phuy4187@gmail.com>
2025-03-17 20:56:02 +0700

    #type event

--- src/components/Calculator/page.tsx
+++ src/components/Calculator/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useMemo, useState } from "react";
+import React, { ChangeEvent, useMemo, useState } from "react";
 import CalculatorInput from "./CalculatorInput";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
@@ -21,9 +21,9 @@ const Calculator = () => {
     setRentDuration(e);
   };
 
-  const handleOnProfitChange = (e: any) => {
+  const handleOnProfitChange = (e: ChangeEvent<HTMLInputElement>): void => {
     e.preventDefault();
-    setProfit(Number(e.target?.value));
+    setProfit(Number(e.target.value));
   };
   return (
     <div className="m-4 flex flex-col space-y-4">
@@ -44,7 +44,7 @@ const Calculator = () => {
       />
       <div>
         <p className="font-[700]">Lãi hàng tháng</p>
-        <Input suffix="%" placeholder="Ví dụ 2.85" onChange={handleOnProfitChange} type="number" />
+        <Input placeholder="Ví dụ 2.85" suffix="%" type="number" onChange={handleOnProfitChange} />
       </div>
       <hr />
       <div className="flex flex-row justify-between">
@@ -63,12 +63,12 @@ const Calculator = () => {
       </div>
       <Button className="w-full">
         Đăng ký ngay{" "}
-        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg fill="none" height="15" viewBox="0 0 15 15" width="15" xmlns="http://www.w3.org/2000/svg">
           <path
+            clip-rule="evenodd"
             d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
             fill="currentColor"
             fill-rule="evenodd"
-            clip-rule="evenodd"
           ></path>
         </svg>
       </Button>
